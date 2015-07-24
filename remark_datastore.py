4c3a1934e4917b1cc19439a8de54e2a74b13c18f
gEvent <gevent@CSSI2015-2.local>
2015-07-24 12:32:18 -0400

    generated random colors

--- remark_datastore.py
+++ remark_datastore.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 from google.appengine.api import memcache
 from google.appengine.ext import ndb
-
+import random
 
 _LAST_GET_KEY_PREFIX = 'lastget'
 _LAST_POST_KEY = 'lastpost'
@@ -34,8 +34,9 @@ def ReadRemarks(user_id):
   remarks = []
   query = Remark.query(Remark.timestamp >= start_time).order(Remark.timestamp)
   for remark in query.fetch():
-    # TODO(cssi-cam-2015) Randomize the color so that each remark is different.
-    remarks.append((remark.user, remark.text, 'black'))
+
+    color = "#%03x" % random.randint(0, 0xFFF)
+    remarks.append((remark.user, remark.text, color))
 
   return remarks
 
